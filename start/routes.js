89c44bf16492279b0e5052bb7853936b8d821a3e
PhuLuong <phult.contact@gmail.com>
2016-11-23 22:41:20 +0700

    implement Home controller

--- start/routes.js
+++ start/routes.js
@@ -1,5 +1,10 @@
 module.exports = function ($route, $logger) {
     /** Register HTTP requests **/
+    $route.get("/", "HomeController@index");
+    $route.get("/device", "HomeController@listDevices");
+    $route.post("/connection", "HomeController@createConnection");
+    $route.get("/connection", "HomeController@listConnections");
+    $route.post("/message", "HomeController@sendMessage");
     /** Register socket.io requests **/
     /** Register filters **/
 };
