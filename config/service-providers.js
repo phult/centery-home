2af818f7b94f59876ea4f19b9d423741db4a3260
PhuLuong <phult.contact@gmail.com>
2016-11-23 22:40:30 +0700

    implement BluetoothConnector

--- config/service-providers.js
+++ config/service-providers.js
@@ -2,9 +2,10 @@ var config = require(__dir + "/core/app/config");
 var routerLoader = require(__dir + "/core/loader/route-loader");
 var event = require(__dir + "/core/app/event");
 var logger = (require(__dir + "/core/log/logger-factory")).getLogger();
+var bluetoothConnector = require(__dir + "/libs/bluetooth/bluetooth-connector");
 module.exports = function ($serviceContainer) {
     $serviceContainer.bind("$config", config);
     $serviceContainer.bind("$route", routerLoader);
     $serviceContainer.bind("$event", event);
-    $serviceContainer.bind("$logger", logger);
+    $serviceContainer.bind("$bluetoothConnector", bluetoothConnector);
 };
